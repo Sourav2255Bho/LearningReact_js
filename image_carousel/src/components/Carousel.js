abbdbc963072b0de1bcc3934f4ba301a69eb395c
Sourav Bhowmick <bhowmicks2255@gmail.com>
2024-06-03 01:19:07 +0530

    Final Commit Done

---
+++ image_carousel/src/components/Carousel.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import "./Carousel.css"
+import { images } from "../helpers/CarouselData"
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+
+
+function Carousel() {
+
+    const [currImg, setCurrImg] = useState(0);
+
+
+  return (
+    <div className='carousel'>
+      <div className='carouselInner' style={{backgroundImage:`url(${images[currImg].img})`}}>
+        <div className='left' onClick={()=>{
+            currImg===0? setCurrImg(images.length-1) : setCurrImg(currImg-1)
+            }}>
+            <ArrowBackIosIcon ></ArrowBackIosIcon>
+        </div>
+        <div className='center'></div>
+        <div className='right' onClick={()=>{
+            currImg===images.length-1? setCurrImg(0) : setCurrImg(currImg+1)
+        }}>
+            <ArrowForwardIosIcon ></ArrowForwardIosIcon>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Carousel
---
+++ image_carousel/src/helpers/CarouselData.js
@@ -0,0 +1,10 @@
+import img1 from "../images/download1.jpeg"
+import img2 from "../images/download2.jpeg"
+import img3 from "../images/download3.jpeg"
+
+
+export const images = [
+    {title : "Nature 1", img : img1},
+    {title : "Nature 2", img : img2},
+    {title : "Nature 3", img : img3}
+]
